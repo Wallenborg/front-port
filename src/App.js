7f912b7d5c76b6bcb9a623d198ef2ae562ee84a3
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-02-11 19:45:21 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import RandomButton from "./components/button/RandomButton";
 import Start from "./pages/Start";
 import ProjectList from "./components/projectBox/ProjectList";
+import Footer from "./components/footer/Footer";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <Start />
       <RandomButton />
       <ProjectList />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      This is a footer, nothing more and nothing less.
+    </footer>
+  );
+}
+
+export default Footer;
