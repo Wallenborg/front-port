a20014197753386b708207c1c06e2b4f153bd730
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-02-15 15:16:26 +0100

    fixed typo

--- src/components/secrettext/SecretText.jsx
+++ src/components/secrettext/SecretText.jsx
@@ -6,7 +6,6 @@ function SecretText() {
       <p className="secret-text">I</p>
       <p className="secret-text">build</p>
       <p className="secret-text">things for</p>
-      <p className="secret-text">for</p>
       <p className="secret-text">the Web !</p>
     </div>
   );
