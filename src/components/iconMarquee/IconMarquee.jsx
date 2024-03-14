a4e8516e1160c180a7f288186d05fc38365534e8
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-03-14 14:46:29 +0100

    update marquee

--- src/components/iconMarquee/IconMarquee.jsx
+++ src/components/iconMarquee/IconMarquee.jsx
@@ -1,10 +1,11 @@
 import Marquee from "react-fast-marquee";
 import "./IconMarquee.css";
 import { FaPeopleGroup, FaCss3Alt } from "react-icons/fa6";
-import { FaReact, FaHtml5, FaTools } from "react-icons/fa";
+import { FaReact, FaHtml5, FaTools, FaGit } from "react-icons/fa";
 import { SiAdobephotoshop, SiAdobeindesign } from "react-icons/si";
 import { GiPaintBrush } from "react-icons/gi";
 import { RiComputerLine } from "react-icons/ri";
+import { IoLogoJavascript } from "react-icons/io";
 
 function IconMarquee() {
   return (
@@ -12,7 +13,9 @@ function IconMarquee() {
       <FaPeopleGroup className="marquee-icon" />
       <FaReact className="marquee-icon" />
       <FaHtml5 className="marquee-icon" />
+      <IoLogoJavascript className="marquee-icon" />
       <FaCss3Alt className="marquee-icon" />
+      <FaGit className="marquee-icon" />
       <SiAdobephotoshop className="marquee-icon" />
       <SiAdobeindesign className="marquee-icon" />
       <FaTools className="marquee-icon" />
